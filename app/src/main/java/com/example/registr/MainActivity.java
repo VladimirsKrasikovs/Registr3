8afb94cf7921991746734934b87cf24a890d6a84
VladimirsKrasikovs <108757262+VladimirsKrasikovs@users.noreply.github.com>
2022-07-25 21:06:39 +0300

    Final

--- app/src/main/java/com/example/registr/MainActivity.java
+++ app/src/main/java/com/example/registr/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.registr;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText username, email, phone, city, password;
+
+    EditText usernameInput;
+    EditText emailInput;
+    EditText cityInput;
+    EditText passwordInput;
+    EditText phoneInput;
+    TextView result;
+
     Button submit;
 
     @Override
@@ -16,18 +28,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        username = (EditText) findViewById(R.id.username);
-        email = (EditText) findViewById(R.id.email);
-        phone = (EditText) findViewById(R.id.phone);
-        city = (EditText) findViewById(R.id.city);
-        password = (EditText) findViewById(R.id.password);
+
+        usernameInput = (EditText) findViewById(R.id.username);
+        emailInput = (EditText) findViewById(R.id.email);
+        phoneInput = (EditText) findViewById(R.id.phone);
+        cityInput = (EditText) findViewById(R.id.city);
+        passwordInput = (EditText) findViewById(R.id.password);
         submit = (Button) findViewById(R.id.submit);
+        result = findViewById(R.id.result);
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                if (usernameInput.getText().toString().isEmpty() || emailInput.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, "Empty text", Toast.LENGTH_SHORT).show();
+                }else{
+                    String username = usernameInput.getText().toString();
+                    String email = emailInput.getText().toString();
+                    String phone = phoneInput.getText().toString();
+                    String city = cityInput.getText().toString();
+                    String password = passwordInput.getText().toString();
+
+                    result.setText("Name is "+username+"\n"+ "Your mail "+email+"\n"+"Your phone number is "+phone+"\n"+ "YOur city is "+ city+"\n"+"Your password is "+password);
+
+
+                }
             }
         });
     }
 }
+
+
+
+
+
+
+
